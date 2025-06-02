d14d9e5735afe26d379a021f5b41d69061ebac6f
Pedro <pedropereirarodrigues16@gmail.com>
2025-06-02 17:05:29 -0300

    yeah...

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { fileURLToPath, pathToFileURL } from 'url';
 config();
 
 const app = express();
-const PORT = process.env.MONITOR_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
